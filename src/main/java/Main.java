d1f774c11ba0aa63330d41c87af85aa31337ea8f
Konrad Suseł <konradsusel@gmail.com>
2018-11-27 16:11:08 +0100

    Changed Main.java to match UML

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        run();
 
+    }
+
+    public static void run() {
         View view = new View();
         String password = view.getPasswordFromUser();
         String login = view.getNickFromUser();
