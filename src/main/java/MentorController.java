d53a608cac5699213fa37189332baeefb0f37c45
Kamil <kamil.m.olsza@gmail.com>
2018-11-27 21:02:58 +0100

    Add Mentor and MentorController

---
+++ src/main/java/MentorController.java
@@ -0,0 +1,36 @@
+public class MentorController{
+
+    private View view;
+    // private DaoMentor dao;
+    public Mentor mentor;
+    // public Map<Integer,Student> students;
+
+    public MentorController(){
+        // this.students = new HashMap<>();
+    }
+
+    public View getView() {
+        return view;
+    }
+
+    public Mentor getMentor() {
+        return mentor;
+    }
+
+
+    public void addStudent(int id){
+
+    }
+
+    public void removeStudent(int id){
+
+    }
+
+    public void editStudentData(int id){
+
+    }
+
+    //    public void addAssigment(){} - to implement
+    //    public void gradeAssigment(){}
+    //    public void checkAttendence(){}
+}
